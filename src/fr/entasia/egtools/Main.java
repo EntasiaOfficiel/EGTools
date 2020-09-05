9b06d110e12edc0b68e996c5e9dc2e8e78873a81
itrooz <team.mortelgames@gmail.com>
2020-09-05 09:58:46 +0200

    added data files deletion at start

--- src/fr/entasia/egtools/Main.java
+++ src/fr/entasia/egtools/Main.java
@@ -9,6 +9,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main extends JavaPlugin{
 
 	public static Main main;
@@ -23,6 +26,7 @@ public class Main extends JavaPlugin{
 		String[] a = main.getConfig().getString("spawn").split(";");
 		Utils.spawn = new Location(Utils.world, Double.parseDouble(a[0])+0.5,Double.parseDouble(a[1])+0.5,Double.parseDouble(a[2])+0.5);
 	}
+
 	@Override
 	public void onEnable() {
 		try{
@@ -44,11 +48,24 @@ public class Main extends JavaPlugin{
 
 			new Task5m().runTaskTimerAsynchronously(this, 1000, 6000); // 5 minutes = 300 secondes = 6000 ticks
 
+			delFiles("playerdata");
+			delFiles("stats");
+			delFiles("advancements");
+
 			getLogger().info("Plugin activé !");
 		}catch(Throwable e){
 			e.printStackTrace();
 			getLogger().info("LE SERVEUR VA S'ARRETER");
 			Bukkit.getServer().shutdown();
+		}
+	}
+
+	public static void delFiles(String dir) throws IOException {
+		File[] files  = new File(Bukkit.getWorlds().get(0).getName()+"/"+dir).listFiles();
+		if(files!=null){
+			for(File lf : files){
+				if(!lf.delete())throw new IOException("File not deleted");
+			}
 		}
 	}
 }
